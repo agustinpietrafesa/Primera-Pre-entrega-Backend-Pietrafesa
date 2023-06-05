ee2ae4d3df177454d9e4066921bf8a82756aad5b
Agustin Pietrafesa <ahpietrafesa@gmail.com>
2023-06-05 17:21:27 -0300

    Finalizadas las validaciones

--- productManager.js
+++ productManager.js
@@ -1,12 +1,17 @@
+/******Creamos la clase que va a manejar nuestros productos ********/
+
 class ProductManager{
     constructor(){
         this.products = [];
     }
 
-
+/******Funcion que nos muestra los productos que manejamos ************/
 getProducts = () => {
     return this.products;   
 };
+
+
+/**********Se crea la funcion que genera el producto con sus respectivos parametros *******/
  
 addProduct = ( title, description, price, thumbnail, code, stock ) => {
      const product = {
@@ -15,22 +20,43 @@ addProduct = ( title, description, price, thumbnail, code, stock ) => {
         price,
         thumbnail,
         code,
-        stock,
-        products: []
+        stock
      }
 
+
+     /***Definimos el ID de nuestros productos, el primero es igual a 1, los siguientes el lenght de nuestro arreglo + 1 ****/ 
      this.products.length === 0
      ?
      product.id = 1 
      :
-     product.id = this.products [ this.products.length - 1 ].id + 1;
+     product.id = this.products.length + 1;
 
 
+     /*******Generamos la obligatoriedad de los parametros ***********/
+     const allParams = [ title, description, price, thumbnail, code, stock ];
+     const reqParams = allParams.every (value => value);
+    
+     if( !reqParams ){
+        console.warn('Ingrese todos los parametros para crear el producto');
+        return;
+     }
+
+
+     /********Generamos la singularidad del code ************/
+
+     const codeExist = this.products.some (product => product.code === code)
+     
+     if(codeExist){
+        console.warn('Este codigo ya esta en el sistema')
+        return;
+     }
+
+     /********Agregamos el producto a nuestra lista de productos mediante el push *******/
      this.products.push(product);
 };
 
 
-
+/*****Funcion que busca productos por el ID ********/
 getProductById = (idProduct) => {
     const productIndex = this.products.findIndex( product => product.id === idProduct );
 
@@ -39,16 +65,19 @@ getProductById = (idProduct) => {
         return;
     }
     
-    const productAdd = this.products [productIndex].products.includes(idProduct);
+
+    const productAdd = () => this.products.includes(idProduct);
 
     if (productAdd) {
-        console.log('El producto se agrego correctamente');
+        console.log(`Se genero un producto con el ID: ${idProduct}`);
         return;
     }
-    this.products[productIndex].products.push(idProduct)
 };
 
 }
+
+
+/******Creamos nuestros primeros productos mediante los metodos previamente definidos******/
 
 
 manageProducts = new ProductManager ();
